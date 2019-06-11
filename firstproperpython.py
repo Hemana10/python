aa3b8880ddd52763416b7f8be7e03ebf89f58ff8
Hemanav <48154794+Hemanav@users.noreply.github.com>
2019-06-11 15:59:52 +0530

    Create firstproperpython.py

---
+++ firstproperpython.py
@@ -0,0 +1,16 @@
+def io():
+    s=input("enter server name")
+    db=input("enter dbname")
+    usr=input("enter usr")
+    pas=input("enter pass")
+    return 'server=%s;dbname=%s;usr=%s;pass=%s'%(s,db,usr,pas)
+def f(b):
+    
+    
+    
+    
+    
+cs=io()
+print(s)
+f(s)
+         
